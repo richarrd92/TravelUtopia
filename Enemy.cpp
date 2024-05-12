3e80f699f0662b0723b797acb3f13e8c5938e542
Richard Maliyetu <160991580+richarrd92@users.noreply.github.com>
2024-05-12 15:12:56 -0400

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,18 +1,3 @@
-/*****************************************
-** File:    Enemy.cpp
-** Project: CMSC 202 Project 4, Spring 2024
-** Author:  Richard Maliyetu
-** Date:    4/06/24
-** Section: 33
-** E-mail:  r215@gl.umbc.edu 
-** E-mail:  r215@umbc.edu 
-**
-** This file contains the implementation of the Enemy class
-** the Enemy class is derived from the class Planet
-** the file contains functions;
-** To get the Create Planet of Enemy Type
-** more info in the project description.
-***********************************************/
 
 #include <iostream>
 #include <string>
