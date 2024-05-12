b0782ee06151934dadf73756982e801d3c6833e2
Richard Maliyetu <160991580+richarrd92@users.noreply.github.com>
2024-05-12 15:13:53 -0400

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,22 +1,3 @@
-/*****************************************
-** File:    Game.cpp
-** Project: CMSC 202 Project 4, Spring 2024
-** Author:  Richard Maliyetu
-** Date:    4/06/24
-** Section: 33
-** E-mail:  r215@gl.umbc.edu 
-** E-mail:  r215@umbc.edu 
-**
-** This file contains the implementation of the Game class
-** the Game class is manages both Ship, Planet and Item classes
-** the file contains functions;
-** To read file, and store given data 
-** To create a ship
-** To Attack a planet
-** To move to another planet
-** To Travel to Utopia
-** more info in the project description.
-***********************************************/
 
 //Includes of required classes
 #include "Ship.h"
